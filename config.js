5f5280c71e74e12ab3518a507f72379a2b0a8cac
Brian Pooe <bp@hhpty.com>
2018-07-26 15:19:29 +0200

    local development with a database

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    client: 'mysql',
+    connection: {
+        host: 'mysql',
+        user: 'test',
+        password: 'test',
+        database: 'test'
+
+    }
+}
--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const config = require("./config");
+const db = require("knex")(config);
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello real time app not working'));
+app.get('/', (req, res) => {
+    db("users").then((data) => {
+        res.send(data);
+    }).catch((err) => console.dir(err));
+});
 
 process.on('SIGINT', () => {
     process.exit();
